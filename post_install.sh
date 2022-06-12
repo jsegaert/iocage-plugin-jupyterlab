301b8e6f7b34a394b1a9e1f101fddc3ba11a651a
Jurgen Segaert <jsegaert@yahoo.com>
2022-06-12 11:56:46 -0700

    Various fixes to post_install.sh file

--- post_install.sh
+++ post_install.sh
@@ -3,26 +3,33 @@
 USER_NAME=jovyan
 USER_UID=1201
 
-# Tweak installation   
+# Tweak installation
 USER_PASS=`pw useradd -n $USER_NAME -u $USER_UID -c JupyterLab -m -s /bin/csh -w random`
+USER_HOME=`eval echo ~$USER_NAME`
 APPL_HASH=`python3.8 -c "from IPython.lib.security import passwd; print(passwd('jupyter'))"`
 
 su $USER_NAME -c "jupyter lab --generate-config"
+su $USER_NAME -c "mkdir -p ~/notebook_dir"
 
 CONFIG_DIR=`su $USER_NAME -c "jupyter --config-dir"`
 CONFIG_FILE="$CONFIG_DIR/jupyter_lab_config.py"
 
 sed -i '' "s/^# c.ServerApp.ip.*/c.ServerApp.ip = '*'/" $CONFIG_FILE
 sed -i '' "s/^# c.ServerApp.open_browser.*/c.ServerApp.open_browser = False/" $CONFIG_FILE
-sed -i '' "s/^# c.ServerApp.password.*/c.ServerApp.password = \"$APPL_HASH\"/" $CONFIG_FILE
+sed -i '' "s/^# c.ServerApp.password.*/c.ServerApp.password = '$APPL_HASH'/" $CONFIG_FILE
+sed -i '' "s|^# c.ServerApp.root_dir.*|c.ServerApp.root_dir = '$USER_HOME/notebook_dir'|" $CONFIG_FILE
 
 # Enable & start the service
 sysrc -f /etc/rc.conf jupyterlab_enable="YES"
 sysrc -f /etc/rc.conf jupyterlab_user="$USER_NAME"
 
-service jupyterlab start  
+service jupyterlab start
 
 # Provide instructions
-echo "The default password for JupyterLab is "\""jupyter"\""" > /root/PLUGIN_INFO
-echo "The password for $USER_NAME is $USER_PASS" > /root/PLUGIN_INFO
+cat <<EOF > /root/PLUGIN_INFO
+The default password for JupyterLab is "jupyter"
+The password for user "$USER_NAME" is "$USER_PASS"
+EOF
+
+cat /root/PLUGIN_INFO
 
