30d7739a7891318979fc012d0ec4bc4f766c4ef3
Jurgen Segaert <jsegaert@yahoo.com>
2022-06-18 17:02:11 -0700

    Change default work_dir in post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -2,16 +2,20 @@
 
 USER_NAME=jovyan
 USER_UID=1201
+WORK_DIR=notebooks
 
 # Tweak installation
 USER_PASS=$(pw useradd -n $USER_NAME -u $USER_UID -c JupyterLab -m -s /bin/csh -w random)
 USER_HOME=$(eval echo ~$USER_NAME)
 APPL_HASH=$(python3.8 -c "from IPython.lib.security import passwd; print(passwd('jupyter'))")
 
+# Update ipykernel - this is a hack to add debugging functionality
 su $USER_NAME -c "pip install --user --no-warn-script-location --upgrade jupyterlab"
 su $USER_NAME -c "pip install --user --no-warn-script-location --upgrade ipykernel"
+
+# Configure JupyterLab
 su $USER_NAME -c "jupyter lab --generate-config"
-su $USER_NAME -c "mkdir -p ~/notebook_dir"
+su $USER_NAME -c "mkdir -p ~/$WORK_DIR"
 
 CONFIG_DIR=$(su $USER_NAME -c "jupyter --config-dir")
 CONFIG_FILE="$CONFIG_DIR/jupyter_lab_config.py"
@@ -19,7 +23,7 @@ CONFIG_FILE="$CONFIG_DIR/jupyter_lab_config.py"
 sed -i '' "s/^# c.ServerApp.ip.*/c.ServerApp.ip = '*'/" $CONFIG_FILE
 sed -i '' "s/^# c.ServerApp.open_browser.*/c.ServerApp.open_browser = False/" $CONFIG_FILE
 sed -i '' "s/^# c.ServerApp.password.*/c.ServerApp.password = '$APPL_HASH'/" $CONFIG_FILE
-sed -i '' "s|^# c.ServerApp.root_dir.*|c.ServerApp.root_dir = '$USER_HOME/notebook_dir'|" $CONFIG_FILE
+sed -i '' "s|^# c.ServerApp.root_dir.*|c.ServerApp.root_dir = '$USER_HOME/$WORK_DIR'|" $CONFIG_FILE
 
 # Enable & start the service
 sysrc -f /etc/rc.conf jupyterlab_enable="YES"
